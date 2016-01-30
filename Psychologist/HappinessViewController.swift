c17e583af418215cf532bedd8f559d83c0b91453
trevor <trevor.jordet@gmail.com>
2016-01-30 12:12:23 -0500

    multiple MVC

---
+++ Psychologist/HappinessViewController.swift
@@ -0,0 +1,57 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by trevor jordet on 1/27/16.
+//  Copyright (c) 2016 jord3t. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController, FaceViewDataSource
+{
+    var happiness: Int = 75 {
+        didSet {
+            happiness = min(max(happiness, 0), 100)
+            println("happiness = \(happiness)")
+            updateUI()
+        }
+    }
+    
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            println("happiness = \(happiness)")
+            faceView.dataSource = self
+            faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: "scale:"))
+        }
+    }
+    
+    private struct Constants {
+    static let HappinessGestureScale: CGFloat = 4
+    }
+    
+    @IBAction func changeHappiness(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .Ended: fallthrough
+        case .Changed:
+            let translation = gesture.translationInView(faceView)
+            let happinessChange =  -Int(translation.y / Constants.HappinessGestureScale)
+            if happinessChange != 0 {
+                happiness += happinessChange
+                gesture.setTranslation(CGPointZero, inView: faceView)
+            }
+        default: break
+        }
+    }
+    
+    
+    private func updateUI()
+    {
+        faceView?.setNeedsDisplay()
+        title = "happiness = \(happiness)"
+    }
+    
+    func smilinessForFaceView(sender: FaceView) -> Double? {
+        return Double(happiness-50)/50
+    }
+}
